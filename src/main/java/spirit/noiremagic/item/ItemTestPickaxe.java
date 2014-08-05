c2007e66aa8424c4b82440843f12a6ca8a6e7d1a
Spirit1292 <Spirit1292@users.noreply.github.com>
2014-08-06 02:02:40 +0300

    Update ItemTestPickaxe.java

--- src/main/java/spirit/noiremagic/item/ItemTestPickaxe.java
+++ src/main/java/spirit/noiremagic/item/ItemTestPickaxe.java
@@ -1,14 +1,11 @@
 package spirit.noiremagic.item;
 
-import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.ItemPickaxe;
-
 public class ItemTestPickaxe extends ItemNM
 {
     public ItemTestPickaxe()
     {
         super();
-        this.setUnlocalizedName("TestPickaxe");
-        this.setCreativeTab(CreativeTabs.tabTools);
+        this.setUnlocalizedName("testPickaxe");
+        this.maxStackSize = 1;
     }
 }
