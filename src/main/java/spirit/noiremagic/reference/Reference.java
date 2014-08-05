463144fd6e04b40b7ec32313cd37d89ff5e509cf
Spirit1292 <Spirit1292@users.noreply.github.com>
2014-08-05 03:35:46 +0300

    Update version of mod to 1.7.10-0.1alpha

--- src/main/java/spirit/noiremagic/reference/Reference.java
+++ src/main/java/spirit/noiremagic/reference/Reference.java
@@ -5,7 +5,7 @@ public class Reference
 	public static final String MOD_ID = "NoireMagic";
 	public static final String MOD_NAME = "Noire Magic";
 	public static final String FINGERPRINT = "@FINGERPRINT@";
-	public static final String VERSION = "0.1a";
+	public static final String VERSION = "1.7.10-0.1alpha";
 	public static final String CLIENT_PROXY_CLASS = "spirit.noiremagic.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "spirit.noiremagic.proxy.ServerProxy";
     public static final String GUI_FACTORY_CLASS = "spirit.noiremagic.client.gui.GuiFactory";
