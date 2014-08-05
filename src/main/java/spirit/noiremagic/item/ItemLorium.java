5433864e55b949827a717024c12627f2f47557d5
Spirit1292 <Spirit1292@users.noreply.github.com>
2014-08-06 02:02:10 +0300

    Update ItemLorium.java

--- src/main/java/spirit/noiremagic/item/ItemLorium.java
+++ src/main/java/spirit/noiremagic/item/ItemLorium.java
@@ -1,14 +1,11 @@
 package spirit.noiremagic.item;
 
-import net.minecraft.creativetab.CreativeTabs;
-
 public class ItemLorium extends ItemNM
 {
     public ItemLorium()
     {
         super();
-        this.setUnlocalizedName("Lorium");
-        this.maxStackSize = 16;
-        this.setCreativeTab(CreativeTabs.tabMisc);
+        this.setUnlocalizedName("lorium");
+        this.maxStackSize = 64;
     }
 }
