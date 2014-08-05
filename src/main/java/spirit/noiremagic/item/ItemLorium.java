68ff17a9e23aed3c239476aa206540976df22693
Spirit1292 <Spirit1292@users.noreply.github.com>
2014-08-05 07:29:17 +0300

    Add some items in mod
    Adding test items - Lorium and TestPickaxe

---
+++ src/main/java/spirit/noiremagic/item/ItemLorium.java
@@ -0,0 +1,14 @@
+package spirit.noiremagic.item;
+
+import net.minecraft.creativetab.CreativeTabs;
+
+public class ItemLorium extends ItemNM
+{
+    public ItemLorium()
+    {
+        super();
+        this.setUnlocalizedName("Lorium");
+        this.maxStackSize = 16;
+        this.setCreativeTab(CreativeTabs.tabMisc);
+    }
+}
---
+++ src/main/java/spirit/noiremagic/item/ItemTestPickaxe.java
@@ -0,0 +1,14 @@
+package spirit.noiremagic.item;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemPickaxe;
+
+public class ItemTestPickaxe extends ItemNM
+{
+    public ItemTestPickaxe()
+    {
+        super();
+        this.setUnlocalizedName("TestPickaxe");
+        this.setCreativeTab(CreativeTabs.tabTools);
+    }
+}
