d4cbefe42f41be1580b0b1eecf2748534baf2ffc
Spirit1292 <Spirit1292@users.noreply.github.com>
2014-08-08 12:28:29 +0300

    Add ResourceLocationHelper.java

---
+++ src/main/java/spirit/noiremagic/utility/ResourceLocationHelper.java
@@ -0,0 +1,17 @@
+package spirit.noiremagic.utility;
+
+import net.minecraft.util.ResourceLocation;
+import spirit.noiremagic.reference.Reference;
+
+public class ResourceLocationHelper
+{
+    public static ResourceLocation getResourceLocation(String modId, String path)
+    {
+        return new ResourceLocation(modId, path);
+    }
+
+    public static ResourceLocation getResourceLocation(String path)
+    {
+        return getResourceLocation(Reference.MOD_ID.toLowerCase(), path);
+    }
+}
