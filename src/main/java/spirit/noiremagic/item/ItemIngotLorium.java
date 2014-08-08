63af9855bc2da42ae377d81dd46d44330c69f37b
Spirit1292 <Spirit1292@users.noreply.github.com>
2014-08-08 12:41:12 +0300

    Add (again) item - Lorium ingot!!!
    How cool it that?!!!

---
+++ src/main/java/spirit/noiremagic/item/ItemIngotLorium.java
@@ -0,0 +1,11 @@
+package spirit.noiremagic.item;
+
+public class ItemIngotLorium extends ItemNM
+{
+    public ItemIngotLorium()
+    {
+        super();
+        this.setUnlocalizedName("ingotLorium");
+        this.maxStackSize = 64;
+    }
+}
